365d5d14e515eb9bde121933affba470ce832b75
Diana <136057217+DianaYT1@users.noreply.github.com>
2024-03-28 15:42:15 +0200

    Add files via upload

---
+++ FacadePattern/FacadePattern/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadePattern
+{
+    public class Program
+    {
+        public class AmusementPark
+        {
+            public bool HasEnoughHigh(Person person,int height)
+            {
+                Console.WriteLine("Check person's high " + person.Height);
+                return true;
+            }
+        }
+
+        public class AgeGroup
+        {
+            public bool HasEnoughYears(Person person,int age)
+            {
+                Console.WriteLine("Check person's age " + person.Age);
+                return true;
+            }
+        }
+
+        public class Amount
+        {
+            public bool HasEnoughMoney(Person person,int amount)
+            {
+                Console.WriteLine("Check person's amount " + person.Amount);
+                return true;
+            }
+        }
+        //'Facade class'
+        public class ParkEnterance
+        {
+            AmusementPark amusementPark = new AmusementPark();
+            AgeGroup ageGroup = new AgeGroup();
+            Amount money = new Amount();
+
+
+            public bool IsEligible(Person person)
+            {
+                Console.WriteLine("{0} wants to enter the park. She need's to be atleast 16 years old, 160 height and to has to have 10$ ",
+                    person.Name);
+
+                bool eligible = true;
+
+                if (!amusementPark.HasEnoughHigh(person, person.Height) || person.Height > 160)
+                {
+                    eligible = false;
+                }
+                else if (!ageGroup.HasEnoughYears(person, person.Age) || person.Age < 16)
+                {
+                    eligible = false;
+                }
+                else if (!money.HasEnoughMoney(person, person.Amount) || person.Amount < 10)
+                {
+                    eligible = false;
+                }
+                return eligible;
+            }
+        }
+        public class Person
+        {
+            private string name;
+            private int height;
+            private int age;
+            private int amount;
+            public Person(string name, int age, int height, int amount)
+            {
+                this.name = name;
+                this.age = age;
+                this.height = height;
+                this.amount = amount;
+            }
+
+            public string Name
+            {
+                get { return name; }
+            }
+
+            public int Height
+            {
+                get { return height; }
+            }
+
+            public int Age
+            {
+                get { return age; }
+            }
+
+            public int Amount
+            {
+                get { return amount; }
+            }
+        }
+        static void Main(string[] args)
+        {
+            ParkEnterance parkEnterance = new ParkEnterance();
+            Person person = new Person("Cortana ",19,160,5);
+            bool eligible = parkEnterance.IsEligible(person);
+
+            Console.WriteLine("\n" + person.Name + "has been " + (eligible ? "Approved" : "Rejected"));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ProxyPattern/ProxyPattern/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyPattern
+{
+    public class Program
+    {
+        public class Solger
+        {
+            public Solger(string name, string role)
+            {
+                Name = name;
+                Role = role;
+            }
+
+            public string Name { get; set; }
+            public string Role { get; set; }
+        }
+
+        public interface IAccessToWeapon
+        {
+            void PerformAccessOperations();
+        }
+
+        public class AccessToWeapon : IAccessToWeapon
+        {
+            public void PerformAccessOperations()
+            {
+                Console.WriteLine("Performing taking a weapon");
+            }
+        }
+
+        public class AccessToWeaponProxy : IAccessToWeapon
+        {
+            private IAccessToWeapon accessToWeapon;
+            private readonly Solger solger;
+
+            public AccessToWeaponProxy(Solger solger)
+            {
+                this.solger = solger;
+            }
+
+            public void PerformAccessOperations()
+            {
+                if(solger.Role.ToUpper() == "ACTIVE" || solger.Role.ToUpper() == "MILITARY")
+                {
+                    accessToWeapon = new AccessToWeapon();
+                    Console.WriteLine("Access for taking a weapon is on");
+                    accessToWeapon.PerformAccessOperations();
+                }
+                else{
+                    Console.WriteLine("Access for taking a weapon is denied");
+                }
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Solger wants to take a weapon");
+            Solger solger1 = new Solger("Liam", "Active");
+            AccessToWeaponProxy solger1Proxy = new AccessToWeaponProxy(solger1);
+            solger1Proxy.PerformAccessOperations();
+        }
+    }
+}
