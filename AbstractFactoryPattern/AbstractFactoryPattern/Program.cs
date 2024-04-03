bb5e13686850e1d3cc78bb09b0431ba0be26759b
Diana <136057217+DianaYT1@users.noreply.github.com>
2024-04-03 11:27:27 +0300

    Adding Abstract Factory and FactoryMethod Patterns

---
+++ AbstractFactoryPattern/AbstractFactoryPattern/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+
+namespace AbstractFactoryPattern
+{
+    public class Program
+    {
+        //'AbstractFactory'
+        abstract class SmartPhoneViewFactory
+        {
+            public abstract Background CreateBackground();
+            public abstract TextColor CreateTextColor();
+        }
+
+        //'ConcreteFactory1'
+        class Mode1 : SmartPhoneViewFactory
+        {
+            public override Background CreateBackground()
+            {
+                return new WhiteBackground();
+            }
+
+            public override TextColor CreateTextColor()
+            {
+                return new DarkTextColor();
+            }
+        }
+
+
+        //'ConcreateFacrory2'
+        class Mode2 : SmartPhoneViewFactory
+        {
+            public override Background CreateBackground()
+            {
+                return new DarkBackground();
+            }
+
+            public override TextColor CreateTextColor()
+            {
+                return new WitheTextColor();
+            }
+        }
+        
+        //'AbstractProductA'
+        abstract class TextColor
+        {
+            
+        }
+
+        //'AbstractProductB'
+        abstract class Background
+        {
+            public abstract void Color(TextColor textColor);
+        }
+
+       
+        //'ProductB1'
+        class DarkBackground : Background
+        {
+             public override void Color(TextColor textColor)
+            {
+                Console.WriteLine(this.GetType().Name + " change the text color to " + textColor.GetType().Name);
+            }
+        } 
+        
+        //'ProductA1'
+        class DarkTextColor : TextColor
+        {
+           
+        }
+        //'ProductB2'
+        class WhiteBackground : Background
+        {
+        public override void Color(TextColor textColor)
+            {
+                Console.WriteLine(this.GetType().Name + " change the text color to " + textColor.GetType().Name);
+            }
+        }
+
+        //'ProductA2'
+        class WitheTextColor : TextColor
+        {
+           
+        }
+
+        //'Client'
+        class SmartPhone
+        {
+            private Background _background;
+            private TextColor _textColor;
+
+            public SmartPhone(SmartPhoneViewFactory factory)
+            {
+                _background = factory.CreateBackground();
+                _textColor = factory.CreateTextColor();
+            }
+
+            public void RunModeChange()
+            {
+
+                _background.Color(_textColor);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            SmartPhoneViewFactory darkmode = new Mode1();
+            SmartPhone smartPhone = new SmartPhone(darkmode);
+            smartPhone.RunModeChange();
+
+
+            SmartPhoneViewFactory whitemode = new Mode2();
+            smartPhone = new SmartPhone(whitemode);
+            smartPhone.RunModeChange();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ FactoryMethodPattern/FactoryMethodPattern/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace FactoryMethodPattern
+{
+    public class Program
+    {
+  
+        static void Main(string[] args)
+        {
+            Creator[] creators = new Creator[2];
+            creators[0] = new CreatorTwo();
+            creators[1] = new CreatorOne();
+
+            foreach(Creator creator in creators)
+            {
+                Animal animal = creator.CreateAnimal();
+                Console.WriteLine("Created {0}", animal.GetType().Name);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    abstract class Animal
+    {
+
+    }
+
+    class Cat: Animal
+    {
+
+    }
+
+    class Dog : Animal
+    {
+
+    }
+
+    abstract class Creator
+    {
+        public abstract Animal CreateAnimal();
+    }
+
+    class CreatorOne:Creator
+    {
+        public override Animal CreateAnimal()
+        {
+            return new Cat();
+        }
+    }
+    class CreatorTwo : Creator
+    {
+        public override Animal CreateAnimal()
+        {
+            return new Dog();
+        }
+    }
+}
